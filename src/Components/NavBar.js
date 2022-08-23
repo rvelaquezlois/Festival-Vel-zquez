1c7f223f00a8d27f8c432bc787f2ed3e37c8fb88
RSV <rvelazquezloisgh@gmail.com>
2022-08-22 21:51:03 -0300

    1.0.2

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return(
+        <header>
+            <h1>MZA Music Festival 2022</h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Lineup</a></li>
+                <li><a href="">Tickets</a></li>
+                <li><a href="">Sobre el festival</a></li>
+            </ul>
+        </nav>
+        </header>
+    );
+}
+
+export default NavBar
